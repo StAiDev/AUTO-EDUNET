e3615ccbd2afda7b6444d64f2a6f80078f1198bb
smartwe <githubdev.smartwe@gmail.com>
2021-11-01 23:14:11 +0900

    .

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ from selenium.webdriver.common.keys import Keys
 import chromedriver_autoinstaller
 import time
 
+username = 'username'
+password = 'password'
+
 chrome_ver = chromedriver_autoinstaller.get_chrome_version().split('.')[0]
 
 try:
@@ -12,4 +15,7 @@ except:
     driver = webdriver.Chrome(f'./{chrome_ver}/chromedriver.exe')
 
 driver.get("https://cls1.edunet.net/")
+driver.find_element_by_id("login_id_main").send_keys(username)
+driver.find_element_by_id("password_main").send_keys(password)
+driver.find_element_by_xpath('//*[@id="loginForm_main"]/div/div[1]/div[2]/button').click()
 driver.implicitly_wait(3)   
