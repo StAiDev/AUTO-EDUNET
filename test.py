afc8d4489698fb5133f8c14f6fd7fb0c7ad569c1
shinyangdevelop <shinyangdevelop@gmail.com>
2021-11-02 19:18:13 +0900

    Imported chromedriver_autoinstaller, and writed code for testing

--- test.py
+++ test.py
@@ -1,3 +1,12 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+import chromedriver_autoinstaller
 
+chrome_ver = chromedriver_autoinstaller.get_chrome_version().split('.')[0]
+
+
+text = "Test String"
+driver = webdriver.Chrome(f'./{chrome_ver}/chromedriver.exe')
+
+driver.get("http://shinyang.shop/steve/test.html")
+driver.find_element_by_xpath('/html/body/form/input[1]').send_keys(text)
