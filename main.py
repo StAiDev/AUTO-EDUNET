de5dbc6174adb9a08faf6f384abf46193c62d570
shinyangdevelop <shinyangdevelop@gmail.com>
2021-11-02 23:12:29 +0900

    Changed path in line 12 and 15

--- main.py
+++ main.py
@@ -9,10 +9,10 @@ password = 'password'
 chrome_ver = chromedriver_autoinstaller.get_chrome_version().split('.')[0]
 
 try:
-    driver = webdriver.Chrome(f'./{chrome_ver}/chromedriver.exe')   
+    driver = webdriver.Chrome(f'/{chrome_ver}/chromedriver.exe')   
 except:
     chromedriver_autoinstaller.install(True)
-    driver = webdriver.Chrome(f'./{chrome_ver}/chromedriver.exe')
+    driver = webdriver.Chrome(f'/{chrome_ver}/chromedriver.exe')
     
 
 driver.get("https://cls1.edunet.net/")
