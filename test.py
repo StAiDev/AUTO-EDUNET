38fffb2ffab185861ab3c57e76e83f9562eacba9
shinyangdevelop <shinyangdevelop@gmail.com>
2021-11-02 19:28:47 +0900

    imported selenium.webdriver.common.by.By, and changed find_element_by_xpath to find_element

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
 import chromedriver_autoinstaller
 
 chrome_ver = chromedriver_autoinstaller.get_chrome_version().split('.')[0]
@@ -9,4 +10,3 @@ text = "Test String"
 driver = webdriver.Chrome(f'./{chrome_ver}/chromedriver.exe')
 
 driver.get("http://shinyang.shop/steve/test.html")
-driver.find_element_by_xpath('/html/body/form/input[1]').send_keys(text)
