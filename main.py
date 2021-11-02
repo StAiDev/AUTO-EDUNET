d7dae170eedc9363a021052e7e7c920c2c246c9b
smartwe <sparklingwee@gmail.com>
2021-11-02 23:46:09 +0900

    .

--- main.py
+++ main.py
@@ -9,15 +9,15 @@ password = 'password'
 chrome_ver = chromedriver_autoinstaller.get_chrome_version().split('.')[0]
 
 try:
-    driver = webdriver.Chrome(f'{chrome_ver}/chromedriver.exe')   
+    driver = webdriver.Chrome(f'./{chrome_ver}/chromedriver.exe')   
 except:
     chromedriver_autoinstaller.install(True)
-    driver = webdriver.Chrome(f'{chrome_ver}/chromedriver.exe')
-    
+    driver = webdriver.Chrome(f'./{chrome_ver}/chromedriver.exe')
 
 driver.get("https://cls1.edunet.net/")
-time.sleep(2)
-driver.find_element_by_xpath('//*[@id="login_id_main"]').send_keys(str(username))
-driver.find_element_by_xpath('//*[@id="password_main"]').send_keys(str(password))
+driver.find_element_by_id("login_id_main").send_keys(username)
+driver.find_element_by_id("password_main").send_keys(password)
 driver.find_element_by_xpath('//*[@id="loginForm_main"]/div/div[1]/div[2]/button').click()
-time.sleep(10)
+time.sleep(2)
+driver.find_element_by_xpath('//*[@id="mCSB_2_container"]/ul/li/a').click()
+time.sleep(1000)
