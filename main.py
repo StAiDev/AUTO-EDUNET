959300e21e7c71eab34fd1cbaedf9980818b4bf0
shinyangdevelop <shinyangdevelop@gmail.com>
2021-11-04 22:26:36 +0900

    Changed some layout

--- main.py
+++ main.py
@@ -4,19 +4,19 @@ import threading
 
 root = Tk()
 root.title("Auto Edunet")
-root.geometry("480x640")
+root.geometry("300x360")
 root.resizable(False, False)
 root.iconbitmap("resources/edunet.ico")
 
 username_label = Label(root, text="ID: ")
 input_username = Entry(root, width=30)
-input_username.grid(column=1, row=0)
-username_label.grid(column=0, row=0)
+input_username.grid(column=1, row=0, pady=10, sticky=N+E+W+S)
+username_label.grid(column=0, row=0, pady=10, sticky=N+E+W+S)
 
 password_label = Label(root, text="Password: ")
 input_password = Entry(root, width=30)
-input_password.grid(column=1, row=1)
-password_label.grid(column=0, row=1)
+input_password.grid(column=1, row=1, sticky=N+E+W+S, pady=10)
+password_label.grid(column=0, row=1, sticky=N+E+W+S, pady=10)
 
 def do():
     username = input_username.get()
@@ -25,6 +25,6 @@ def do():
     thread.start()
 
 btn1 = Button(root, width=20, height=2, text="Launch", command=do)
-btn1.grid(column=0, row=2, columnspan=2)
+btn1.grid(column=0, row=2, columnspan=2, sticky=N+E+W+S)
 
 root.mainloop()
