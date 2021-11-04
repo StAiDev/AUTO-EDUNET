07c8654cc94ece4e0756abefbc77914319592ada
shinyangdevelop <shinyangdevelop@gmail.com>
2021-11-04 18:39:29 +0900

    Made button at main.py, but not working because auto_e.py automatically start

--- main.py
+++ main.py
@@ -7,4 +7,15 @@ root.geometry("480x640")
 root.resizable(False, False)
 
 
+def do(userid, password):
+    modules.auto_e.run(userid, password)
+
+
+f = open("login.txt")
+userid = f.readline()
+password = f.readline()
+
+btn1 = Button(root, width=20, height=2, text="Launch", command=do(userid, password))
+btn1.pack()
+
 root.mainloop()
