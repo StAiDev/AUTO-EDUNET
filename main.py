1ee13812644d677fd1ba8d935d33a1b46670d225
shinyangdevelop <shinyangdevelop@gmail.com>
2021-11-04 21:43:49 +0900

    Added basic GUI in main.py

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
 from tkinter import *
-import modules.auto_e
-
+from modules import auto_e
 root = Tk()
 root.title("Auto Edunet")
 root.geometry("480x640")
 root.resizable(False, False)
 
+input_username = Entry(root, width=30)
+input_username.pack()
 
+input_password = Entry(root, width=30)
+input_password.pack()
+
+def do():
+    username = input_username.get()
+    password = input_password.get()
+    auto_e.run(username, password)
+
+btn1 = Button(root, width=20, height=2, text="Launch", command=do)
+btn1.pack()
 
 root.mainloop()
