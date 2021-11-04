4819f81322ede459545d16f6297cafcf693fb340
shinyangdevelop <shinyangdevelop@gmail.com>
2021-11-04 17:38:22 +0900

    Deleted unnecessary imports from main.py

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
 from tkinter import *
-from modules.auto_e import run
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.common.exceptions import NoSuchElementException
-from selenium.webdriver import ActionChains
-from selenium.webdriver.common.by import By
-import chromedriver_autoinstaller
-import time
+import modules.auto_e
 
 root = Tk()
 root.title("Auto Edunet")
@@ -14,15 +7,4 @@ root.geometry("480x640")
 root.resizable(False, False)
 
 
-
-f = open("login.txt", 'r')
-
-username = f.readline()
-password = f.readline()
-
-def do():
-    root.mainloop()
-    run(username, password)
-    print("suc")
-
-do()
+root.mainloop()
